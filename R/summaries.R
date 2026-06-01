729cad318e0e8f11bce39d8fd7b1d2d81400246d
Cameron An <cameronan03@outlook.com>
2026-06-01 16:34:40 -0700

    Added to @importFrom to include group_by and mutate functions

--- R/summaries.R
+++ R/summaries.R
@@ -2,7 +2,7 @@
 #'
 #' @param grouping_var A column to group by (unquoted).
 #' @return A tibble with counts of each rarity by group.
-#' @importFrom dplyr count
+#' @importFrom dplyr count group_by mutate
 #' @importFrom tidyr pivot_wider
 #' @export
 rarity_by_release <- function(grouping_var) {
@@ -10,10 +10,10 @@ rarity_by_release <- function(grouping_var) {
   dat <- load_data()
 
   dat |>
-    count({{grouping_var}}, rarity) |>
-    group_by({{grouping_var}}) |>
-    mutate(pct = n / sum(n)) |>
-    pivot_wider(
+    dplyr::count({{grouping_var}}, rarity) |>
+    dplyr::group_by({{grouping_var}}) |>
+    dplyr::mutate(pct = n / sum(n)) |>
+    tidyr::pivot_wider(
       names_from = rarity,
       values_from = pct
     )
